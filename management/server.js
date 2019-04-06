b2a303db53bbf16de60f741b1ce7e4bd6bb2020d
Jessie Gayeon Ko <gayeonko@hotmail.com>
2019-04-06 12:03:04 -0400

    typo  img address, mysql table is case sensitive

--- management/server.js
+++ management/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs'); // library that can aceess the file
+const mysql = require('mysql'); // call mysql library
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +8,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json'); //read file
+const conf = JSON.parse(data); //data parsing
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': 'James Brouwn',
-            'birthday': '991023',
-            'sex': 'Male',
-            'job': 'Student'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': 'Michelle Obama',
-            'birthday': '771103',
-            'sex': 'Female',
-            'job': 'Formal first lady'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': 'Ariana Grande',
-            'birthday': '891023',
-            'sex': 'Female',
-            'job': 'Singer'
-          }
-    ]);
+   connection.query(
+     "SELECT * FROM customer",
+     (err, rows, fields) => {
+       res.send(rows);
+     }
+   )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
