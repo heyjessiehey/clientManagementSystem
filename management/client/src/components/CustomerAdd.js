d71231dd9cbd7a984053f7eb606338c8bae30018
Jessie Gayeon Ko <gayeonko@hotmail.com>
2019-04-28 23:11:10 -0400

    Add Material UI Design for Add/Delete Customer

--- management/client/src/components/CustomerAdd.js
+++ management/client/src/components/CustomerAdd.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+})
 
 class CustomerAdd extends React.Component {
     
@@ -11,7 +25,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             sex: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         }
     }
 
@@ -28,7 +43,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             sex: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
 
     }
@@ -62,19 +78,54 @@ class CustomerAdd extends React.Component {
         return post(url, formData, config);
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            sex: '',
+            job: '',
+            fileName: '',
+            open: false
+        })
+    }
+
     render() {
+        const { classes } = this.props;
         return(
-            <form onSubmit = {this.handleFormSubmit}>
-                <h1>Add Client</h1>
-                Profile Image: <input type="file" name ="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}></input><br></br>
-                Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}></input><br></br>
-                Birthday: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}></input><br></br>
-                Sex: <input type="text" name="sex" value={this.state.sex} onChange={this.handleValueChange}></input><br></br>
-                Job: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}></input><br></br>
-                <button type="submit">Add</button>
-            </form>
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    Add Client
+                </Button>
+                <Dialog open={this.state.open}  onClose={this.handleClose}>
+                    <DialogTitle>Add Client</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br></br>
+                        <label htmlFor="raised-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "Choose Profile Image" : this.state.fileName}
+                            </Button>
+                        </label>
+                        <br/>
+                        <TextField label="Name" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}></TextField><br></br>
+                        <TextField label="Birthday" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}></TextField><br></br>
+                        <TextField label="Sex" type="text" name="sex" value={this.state.sex} onChange={this.handleValueChange}></TextField><br></br>
+                        <TextField label="Job" ype="text" name="job" value={this.state.job} onChange={this.handleValueChange}></TextField><br></br>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>Add</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>Close</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
